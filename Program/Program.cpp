a9faa571b3e05f3e677c8592ad38e8ad9bafe79b
Semchenok <Semchenok.kirill@gmail.com>
2018-03-14 08:38:59 +0300

    3_commit

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,11 +1,20 @@
-// Program.cpp: определяет точку входа для консольного приложения.
-//
-
 #include "stdafx.h"
+#include "footwear.h"
 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	return 0;
+	const int N=2;
+	footwear ArrayFootwear[N];
+	ArrayFootwear[1].setManufacturer("Hoodlab");
+	ArrayFootwear[1].setSize(41.5);
+	ArrayFootwear[1].setPrice(55.00);
+	ArrayFootwear[1].print();
+	ArrayFootwear[2].setManufacturer("M+RC Noir");
+	ArrayFootwear[2].setSize(37.5);
+	ArrayFootwear[2].setPrice(45.75);
+	ArrayFootwear[2].print();
+	system("pause");
+    return 0;
 }
 
